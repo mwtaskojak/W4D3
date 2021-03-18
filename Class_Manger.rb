0586d707a5ddf55f7d20746a2664795b04becff7
mwtas <mwtasemalkojak@gmail.com>
2021-03-17 20:04:35 -0400

    Creat  class Manager  < Employee

--- Class_Manger.rb
+++ Class_Manger.rb
@@ -35,6 +35,6 @@ class Manager  < Employee
 
 end 
 
- p ned.bonus(5) # => 500_000
- p darren.bonus(4) # => 88_000
- p david.bonus(3) # => 30_000
+#  p ned.bonus(5) # => 500_000
+#  p darren.bonus(4) # => 88_000
+#  p david.bonus(3) # => 30_000
